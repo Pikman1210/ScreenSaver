849a9678f8e5415bf5cc6cfd061a506a30c9841d
Pikman1210 <72507796+Pikman1210@users.noreply.github.com>
2023-06-10 17:33:48 -0500

    Time and Date

--- Assets/Scripts/SystemTimeDisplay.cs
+++ Assets/Scripts/SystemTimeDisplay.cs
@@ -6,15 +6,22 @@ using TMPro;
 public class SystemTimeDisplay : MonoBehaviour
 {
 
-    public TextMeshProUGUI text;
+    public TextMeshProUGUI timeText;
+    public TextMeshProUGUI dateText;
 
     private int hour;
     private int minute;
     private int second;
 
+    private int month;
+    private int day;
+    private int year;
+
     void Start()
     {
-        
+        month = System.DateTime.Now.Month;
+        day = System.DateTime.Now.Day;
+        year = System.DateTime.Now.Year;
     }
 
     void Update()
@@ -22,6 +29,36 @@ public class SystemTimeDisplay : MonoBehaviour
         hour = System.DateTime.Now.Hour;
         minute = System.DateTime.Now.Minute;
         second = System.DateTime.Now.Second;
-        text.SetText(hour + ":" + minute + ":" + second);
+
+        if (second < 10 && minute < 10)
+        {
+            timeText.SetText(hour + ":0" + minute + ":0" + second);
+        } else if (minute < 10)
+        {
+            timeText.SetText(hour + ":0" + minute + ":" + second);
+        } else if (second < 10)
+        {
+            timeText.SetText(hour + ":" + minute + ":0" + second);
+        } else
+        {
+            timeText.SetText(hour + ":" + minute + ":" + second);
+        }
+
+        if (day < 10 && month < 10)
+        {
+            dateText.SetText("0" + month + "-0" + day + "-" + year);
+        }
+        else if (month < 10)
+        {
+            dateText.SetText("0" + month + "-" + day + "-" + year);
+        }
+        else if (day < 10)
+        {
+            dateText.SetText(month + "-0" + day + "-" + year);
+        }
+        else
+        {
+            dateText.SetText(month + "-" + day + "-" + year);
+        }
     }
 }
